56d03bf030a872a699dec94d92c8dc291553e558
Sippos <63863989+Sippos@users.noreply.github.com>
2026-05-30 02:19:44 +0200

    Route games to dedicated page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Movies from './pages/Movies.jsx'
 import Series from './pages/Series.jsx'
+import Games from './pages/Games.jsx'
 import Videos from './pages/Videos.jsx'
-import MediaPage from './pages/MediaPage.jsx'
 import Leaderboard from './pages/Leaderboard.jsx'
-import { demoGames } from './lib/demoMovies.js'
 
 export default function App() {
   return (
@@ -13,7 +12,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/movies" element={<Movies />} />
       <Route path="/series" element={<Series />} />
-      <Route path="/games" element={<MediaPage active="games" eyebrow="Games" title="Games to play next" description="Collect party games, co-op campaigns, and solo recommendations worth trying. Search API will be added later." items={demoGames} itemLabel="games" likeLabel="Play" historyLabel="Played games" />} />
+      <Route path="/games" element={<Games />} />
       <Route path="/videos" element={<Videos />} />
       <Route path="/leaderboard" element={<Leaderboard />} />
     </Routes>
