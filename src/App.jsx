3f344883b9bf27e01be4a5eee79e476fea54496e
Sippos <63863989+Sippos@users.noreply.github.com>
2026-05-30 02:31:07 +0200

    Wire music and group routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Movies from './pages/Movies.jsx'
 import Series from './pages/Series.jsx'
 import Games from './pages/Games.jsx'
 import Videos from './pages/Videos.jsx'
+import Music from './pages/Music.jsx'
+import Groups from './pages/Groups.jsx'
 import Leaderboard from './pages/Leaderboard.jsx'
 
 export default function App() {
@@ -14,6 +16,10 @@ export default function App() {
       <Route path="/series" element={<Series />} />
       <Route path="/games" element={<Games />} />
       <Route path="/videos" element={<Videos />} />
+      <Route path="/music" element={<Music />} />
+      <Route path="/groups" element={<Groups />} />
+      <Route path="/g/:groupId" element={<Groups />} />
+      <Route path="/invite/:code" element={<Groups inviteMode />} />
       <Route path="/leaderboard" element={<Leaderboard />} />
     </Routes>
   )
