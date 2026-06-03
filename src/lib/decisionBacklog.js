c1137b9e2850b9b7ce0a2d276fb6795828ae6f54
Sippos <63863989+Sippos@users.noreply.github.com>
2026-06-03 09:27:52 +0200

    Add decision backlog option builder

---
+++ src/lib/decisionBacklog.js
@@ -0,0 +1,27 @@
+import { getGames, getMovies, getSeries } from './supabaseClient.js'
+
+function scoreItem(item) {
+  return Number(item.score || 0) * 10 + Number(item.picks || 0)
+}
+
+function structuredOption(type, item) {
+  return `[${type}:${item.id}] ${item.title}`
+}
+
+export async function getDecisionBacklogOptions(groupId, limit = 6) {
+  if (!groupId) return []
+  const [movies, series, games] = await Promise.all([
+    getMovies(groupId).catch(() => []),
+    getSeries(groupId).catch(() => []),
+    getGames(groupId).catch(() => []),
+  ])
+
+  return [
+    ...movies.filter((item) => !item.watched).map((item) => ({ type: 'movie', item })),
+    ...series.filter((item) => !item.finished).map((item) => ({ type: 'series', item })),
+    ...games.filter((item) => !item.played).map((item) => ({ type: 'game', item })),
+  ]
+    .sort((a, b) => scoreItem(b.item) - scoreItem(a.item) || String(a.item.title).localeCompare(String(b.item.title)))
+    .slice(0, limit)
+    .map(({ type, item }) => structuredOption(type, item))
+}
